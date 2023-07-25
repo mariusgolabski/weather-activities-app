06097182d64c4d61d09d88344701004f3fd6a489
Jonas Dally <jonas.dall@hotmail.de>
2023-07-25 10:43:57 +0200

    add activity-list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import ActivityItem from "./components/ActivityItem";
+import ActivityList from "./components/ActivityList";
 import Form from "./components/Form";
 
 function App() {
   return (
     <div className="App">
+      <ActivityList />
       <Form />
     </div>
   );
---
+++ src/components/ActivityItem/index.js
@@ -0,0 +1,12 @@
+import "./ActivityItem.css";
+
+export default function ActivityItem() {
+  return(
+    <>
+  <li>asdasd</li>
+  <li>asdadfasd</li>
+  <li>adfga</li>
+
+  </>
+  )
+};
