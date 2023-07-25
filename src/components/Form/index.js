507782e0f3dd80911e2396b54e2de3c001179229
Kevin Kyaterekera <kn.kyaterekera@gmail.com>
2023-07-25 13:01:28 +0200

    added isCheckedFuntion

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,17 +1,24 @@
+import useLocalStorageState from "use-local-storage-state";
 import Button from "../Button";
 import "./Form.css";
 
 export default function Form({ onAddActivity }) {
-  function handleAddActivity(event) {
+  function handleSubmitButton(event) {
     event.preventDefault();
     const formData = new FormData(event.target);
     const data = Object.fromEntries(formData);
     onAddActivity(data);
   }
+
+  const [isChecked, setIsChecked] = useLocalStorageState(false);
+  function handleChange(event) {
+    setIsChecked(event.target.checked);
+  }
+
   return (
     <>
       <h1>Add new activity</h1>
-      <form onSubmit={handleAddActivity}>
+      <form onSubmit={handleSubmitButton}>
         <div>
           <label htmlFor="activity-input">Name:</label>
           <input type="text" id="activity-input" name="name" required></input>
@@ -19,6 +26,7 @@ export default function Form({ onAddActivity }) {
         <div>
           <label htmlFor="activity-check">Good-weather activity:</label>
           <input
+            onChange={handleChange}
             type="checkbox"
             id="activity-check"
             name="isForGoodWeather"
