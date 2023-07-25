df69461ffe0b432055c55fc692e2f31d9b9b992a
Kevin Kyaterekera <kn.kyaterekera@gmail.com>
2023-07-25 12:15:11 +0200

    feature: add handleActivity

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,18 +1,28 @@
 import Button from "../Button";
 import "./Form.css";
 
-export default function Form() {
+export default function Form({ onAddActivity }) {
+  function handleAddActivity(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+    onAddActivity(data);
+  }
   return (
     <>
       <h1>Add new activity</h1>
-      <form>
+      <form onSubmit={handleAddActivity}>
         <div>
           <label htmlFor="activity-input">Name:</label>
-          <input type="text" id="activity-input" required></input>
+          <input type="text" id="activity-input" name="name" required></input>
         </div>
         <div>
           <label htmlFor="activity-check">Good-weather activity:</label>
-          <input type="checkbox" id="activity-check" required></input>
+          <input
+            type="checkbox"
+            id="activity-check"
+            name="isForGoodWeather"
+          ></input>
         </div>
         <Button />
       </form>
